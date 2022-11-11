b2bddae5c7d6df5135ef26db872eff6b981abfb3
Quiquw <salvadorenrique.aa@gmail.com>
2022-11-11 01:13:27 +0100

    last changes

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
 var awa = "Nombre de usuario: ";
 var sockets = [];
 
- app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/index.html');
- });
-
- app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/chat.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-    io.on('connection', (socket) => {
-      socket.on('user connection', (userAppName) => {
-  //      if (socket not in stack) {
-          socket.broadcast.emit('newUser', { id: socket.id, username: username });
-          sockets.push({ id: socket.id, userAppName: userAppName });
-          console.log(sockets);
-   //     }
-   // array sockets conectados
-      });
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
 
-       socket.on('mensajeGeneral', (msg) => {
-        console.log("mensaje en el chat general -- " + msg);
-        socket.broadcast.emit('mensajeGeneral', msg);
-      });
-
-      socket.on('mensajeNoGeneral', (msg2) => {
-        console.log("mensaje en el chat NO general -- " + msg2);
-        socket.broadcast.emit('mensajeNoGeneral', msg2);
-      });
-  
-
-    socket.on('mensajeDirecto', ({ privateMessage, reciever }) => {
-      console.log("mendaje directo: " + privateMessage);
-      socket.to(reciever).emit("mensaje directo", privateMessage);
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  console.log("");
+  console.log('se conecto socket');
+  console.log(sockets.length);
+  socket.on("user connection", (userAppName) => {
+    socket.broadcast.emit("newUser", {
+      id: socket.id,
+      userAppName: userAppName
     });
+    sockets.push({ id: socket.id, userAppName: userAppName });
+    console.log('array sockets'+JSON.stringify(sockets));
+  });
 
-    socket.on('disconect', () => {
-      // implementar desconexion
-        console.log("se desconectó");
-        // array sockets conectados
+  socket.on("mensajeGeneral", (msg) => {
+    console.log("mensaje en el chat general -- " + msg);
+    socket.broadcast.emit("mensajeGeneral", msg);
+  });
+
+  socket.on("mensajeNoGeneral", (msg2) => {
+    console.log("mensaje en el chat NO general -- " + msg2);
+    socket.broadcast.emit("mensajeNoGeneral", msg2);
+  });
+
+  socket.on("mensajeDirecto", ({ privateMessage, reciever }) => {
+    console.log("mendaje directo: " + privateMessage);
+    socket.to(reciever).emit("mensaje directo", privateMessage);
+  });
+
+  socket.on("disconnect", () => {
+    sockets = sockets.filter((theSocket) => theSocket.id !== socket.id);
+    console.log("se desconectó " + JSON.stringify(sockets));
+   // socket.broadcast.emit('disconnected', userAppName);
   });
 });
-
-function refreshUsers (){
-}
-
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
